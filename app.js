7b091ccc144e80ff528096723fbaadae70f9521f
Sanket Bhangale <sanketbhangale3918@gmail.com>
2024-07-10 10:34:37 -0500

    set up geolocation and realtime location update

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const path = require('path');
 const http = require('http');
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const socket = require('socket.io');
 const server = http.createServer(app);
 const io = socket(server);
+
+io.on('connection', function(socket){
+    socket.on('send-location', function(data){
+        io.emit('receive-location', { id: socket.id, ...data });
+    });
+    socket.on('disconnect', function(){
+        io.emit('user-disconnected', socket.id);
+    })
+});
 
 app.get('/', function(req,res){
 res.render('index');
